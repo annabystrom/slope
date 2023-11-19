8a61a2c54a309f37d951268225a49db278fbd736
aesok <aesok@dol.ru>
2023-11-19 12:20:50 +0300

    make widgets demo gtk4 compatible

--- demos/widgets.c
+++ demos/widgets.c
@@ -39,7 +39,9 @@ gboolean update_plot(GtkWidget *button, gpointer data)
   return TRUE;
 }
 
-int main(int argc, char *argv[])
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
 {
   GtkWidget *  window;
   GtkWidget *  box;
@@ -48,25 +50,19 @@ int main(int argc, char *argv[])
   SlopeScale * scale;
   SlopeItem *  series;
 
-  gtk_init(&argc, &argv);
-
   /* create window layout */
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  window = gtk_application_window_new (app);
   box    = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
   button = gtk_button_new_with_label("Update plot");
   view   = slope_view_new();
   figure = slope_figure_new();
   gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
-  g_signal_connect(
-      G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(update_plot), NULL);
-  gtk_container_add(GTK_CONTAINER(window), box);
-  gtk_box_pack_start(GTK_BOX(box), view, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(box), button, FALSE, TRUE, 2);
+  gtk_window_set_child (GTK_WINDOW (window), box);
+  gtk_box_append (GTK_BOX(box), view);
+  gtk_box_append (GTK_BOX(box), button);
   slope_view_set_figure(SLOPE_VIEW(view), figure);
 
-  x = g_malloc(n * sizeof(double));
-  y = g_malloc(n * sizeof(double));
   update_plot(button, NULL);
 
   scale = slope_xyscale_new();
@@ -75,11 +71,25 @@ int main(int argc, char *argv[])
   series = slope_xyseries_new_filled("Random Points", x, y, n, "kor");
   slope_scale_add_item(scale, series);
 
-  gtk_widget_show_all(window);
-  gtk_main();
+  gtk_window_present (GTK_WINDOW (window));
+  update_plot(button, NULL);
+}
+
+int main(int argc, char *argv[])
+{
+  GtkApplication *app;
+  int status = 0;
+
+  x = g_malloc(n * sizeof(double));
+  y = g_malloc(n * sizeof(double));
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
   g_free(x);
   g_free(y);
 
-  return 0;
+  return status;
 }
