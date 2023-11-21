7e8f7fd7e74769e1d8c657cd1521bf89ded8eb7a
aesok <aesok@dol.ru>
2023-11-21 22:38:13 +0300

    Make "plot_stack" demo gtk4 compatible

--- demos/plot_stack.c
+++ demos/plot_stack.c
@@ -21,35 +21,22 @@
 #include <math.h>
 #include <slope/slope.h>
 
-int main(int argc, char *argv[])
-{
-  GtkWidget *   chart;
-  SlopeScale *  scale1, *scale2;
-  SlopeItem *   series11, *series12, *series2;
-  SlopeItem *   axis;
-  SlopeSampler *sampler;
-  double *      x, *y11, *y12, *y2;
+GtkWidget *   chart;
+SlopeScale *  scale1, *scale2;
+SlopeItem *   series11, *series12, *series2;
+SlopeItem *   axis;
+SlopeSampler *sampler;
 
-  gtk_init(&argc, &argv);
+double *      x, *y11, *y12, *y2;
+int n = 50;
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
   chart = slope_chart_new();
 
-  g_signal_connect(G_OBJECT(chart), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-  /* create some sinusoidal data points */
-  long k, n = 50;
-  x         = g_malloc(n * sizeof(double));
-  y11       = g_malloc(n * sizeof(double));
-  y12       = g_malloc(n * sizeof(double));
-  y2        = g_malloc(n * sizeof(double));
-  double dx = 4.0 * G_PI / n;
-
-  for (k = 0; k < n; ++k)
-    {
-      x[k]   = k * dx;
-      y11[k] = sin(x[k]);
-      y12[k] = cos(x[k]);
-      y2[k]  = 1.0 + y11[k] + 0.1 * k;
-    }
+  gtk_application_add_window (app, GTK_WINDOW (chart));
 
   scale1 = slope_xyscale_new_axis("Phase", "Amplitude", "Sinusoidal functions");
   slope_scale_set_layout_rect(scale1, 0, 0, 1, 1);
@@ -75,8 +62,34 @@ int main(int argc, char *argv[])
   series2 = slope_xyseries_new_filled("Sine + Linear", x, y2, n, "kob");
   slope_scale_add_item(scale2, series2);
 
-  gtk_widget_show_all(chart);
-  gtk_main();
+  gtk_window_present (GTK_WINDOW (chart));
+}
+
+int main(int argc, char *argv[])
+{
+  GtkApplication *app;
+  int status = 0;
+
+  /* create some sinusoidal data points */
+  long k;
+  x         = g_malloc(n * sizeof(double));
+  y11       = g_malloc(n * sizeof(double));
+  y12       = g_malloc(n * sizeof(double));
+  y2        = g_malloc(n * sizeof(double));
+  double dx = 4.0 * G_PI / n;
+
+  for (k = 0; k < n; ++k)
+    {
+      x[k]   = k * dx;
+      y11[k] = sin(x[k]);
+      y12[k] = cos(x[k]);
+      y2[k]  = 1.0 + y11[k] + 0.1 * k;
+    }
+
+  app = gtk_application_new ("slope.plot_stack", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
   g_free(x);
   g_free(y11);
